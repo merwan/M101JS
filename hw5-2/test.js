0fd5754775ab2da8d78fd408a7ae4fa7ca932e76
Merouane Atig <merouane.atig@gmail.com>
2015-09-07 16:02:12 +0200

    Summ all averages for hw5-2

--- hw5-2/test.js
+++ hw5-2/test.js
@@ -2,5 +2,5 @@ use test
 
 db.zips.aggregate([
     {$match: {'state': {$in: ['CA', 'NY']}, 'pop': {$gt: 25000}}},
-    {$group: {_id: '$state', average: {$avg: '$pop'}}}
+    {$group: {_id: null, average: {$avg: '$pop'}}}
     ]);
