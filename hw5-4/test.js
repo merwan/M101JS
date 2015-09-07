6b2fb7236423b2490ec1c9f56527687aff89ae9b
Merouane Atig <merouane.atig@gmail.com>
2015-09-07 15:33:01 +0200

    Add approximate solution to hw5-4

---
+++ hw5-4/test.js
@@ -0,0 +1,12 @@
+use test
+
+db.zips.aggregate([
+    {$project:
+      {
+        first_char: {$substr : ["$city",0,1]},
+        pop: 1
+      }
+    },
+    {$match: {'first_char': {$in: [/[0-9]/]}}},
+    {$group: {_id: '$first_char', sum: {$sum: '$pop'}}}
+])
